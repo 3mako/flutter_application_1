4b70130423e2746eccdc983c72256544fd37d71d
miki <jaxrgmay@gmail.com>
2023-05-25 14:35:28 +0900

    upgrade

---
+++ lib/losspg.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class losspg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('削減量'),
+      ),
+      body: Center(
+        child: Text('削減しろ'),
+      ),
+    );
+  }
+}
